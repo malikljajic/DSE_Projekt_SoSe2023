4b0d2ae3ee52addc045e5fb36a25133c926cb7c4
Malik Ljajic <86000813+malikljajic@users.noreply.github.com>
2023-07-02 21:28:15 +0200

    Basis Code und CSV Rohdaten

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# CSV-Datei einlesen
+df = pd.read_csv('Datenbasis\Krebsdaten.csv',encoding="latin-1", sep=';', decimal=',', index_col='Krebsart')
+# Report erstellen
+#report = df.describe()
+df.sort_index()
+#größte Werte ausgeben
+#fivelargestvalues=df.nlargest(5,['2008'])
+fivelargestvalues = df.sort_values(by=['2008'])
+# Report anzeigen
+print(fivelargestvalues.head(5))
