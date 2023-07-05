3598a5b2d4dda3280d952d2a229517cf4e106d21
Malik Ljajic <malik.ljajic@student.hnu.de>
2023-07-06 01:05:46 +0200

    Grundgerüst des Dashboards angepasst main.py Methode für best of 5 geschrieben. Diese muss später noch in die app.py inkludiert werden. Leider immer noch fehlerhaft.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 from dash.dependencies import Input, Output
 
 # Incorporate data
-df = pd.read_csv('Datenbasis/Krebsdaten.csv', encoding="latin-1", sep=';', decimal=',', index_col='Krebsart')
+df = pd.read_csv('Datenbasis/Krebsdaten.csv', encoding="latin-1", sep=';', decimal=',')
 
 # Initialize the app
 app = dash.Dash(__name__)
@@ -19,29 +19,12 @@ app.layout = html.Div([
     html.H1('Krebsdashboard Deutschland'),
     html.H2('Zahlen pro 100 000 Einwohner'),
 
-    html.Div([
-        html.Label('Sort by:'),
-        dcc.Dropdown(
-            id='sort-dropdown',
-            options=[
-                {'label': 'Krebsart', 'value': 'Krebsart'},
-                {'label': 'Alter', 'value': 'Alter'},
-                {'label': 'Jahr', 'value': 'Jahr'},
-                {'label': 'Geschlecht', 'value': 'Geschlecht'}
-            ],
-            value='Krebsart',
-            clearable=False
-        ),
-    ]),
-
-    dash_table.DataTable(
+dash_table.DataTable(
         id='datatable',
         data=df.to_dict('records'),
         columns=[{'name': col, 'id': col} for col in df.columns],
         page_size=5,
     ),
-
-    dcc.Graph(id='graph')
 ])
 
 
@@ -53,6 +36,7 @@ app.layout = html.Div([
 def update_table(filter_value, sort_value):
     var = df[df['Krebsart'].str.contains(filter_value)]
     return var.to_dict('records')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8051)
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd
 
 # CSV-Datei einlesen
-df = pd.read_csv('Datenbasis\Krebsdaten.csv', encoding="latin-1", sep=';', decimal=',', index_col='Krebsart')
-# Report erstellen
-# report = df.describe()
-df.sort_index()
-# größte Werte ausgeben
-# fivelargestvalues=df.nlargest(5,['2008'])
-fivelargestvalues = df.sort_values(by=['2008'])
-# Report anzeigen
-print(fivelargestvalues.head(5))
-print('TestArezo')
+df = pd.read_csv('Datenbasis/Krebsdaten.csv', encoding="latin-1", sep=';', decimal=',')
 
+# Filter für die gewünschten Jahre erstellen
+jahre = ['2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']
+filtered_df = df.copy()
+
+# Die 5 höchsten Werte für jede Spalte ermitteln
+#lamda bedeutet auf jede Spalte umsetzen
+filtered_df[jahre] = filtered_df[jahre].apply(lambda x: pd.to_numeric(x, errors='coerce'))
+
+# Spalten in den Datentyp 'float' umwandeln
+top_5_values = filtered_df[jahre].max().sort_values(ascending=False)[:5]
+
+# Ergebnis anzeigen
+print(top_5_values)
