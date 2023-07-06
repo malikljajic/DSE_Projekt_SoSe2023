ab033a2045fba3913356fc50238785bfa6bf5f7f
Malik Ljajic <malik.ljajic@student.hnu.de>
2023-07-06 12:28:21 +0200

    Weiteren Filter hinzugefügt. Leider immer noch fehlerhaft.

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import pandas as pd
 # CSV-Datei einlesen
 df = pd.read_csv('Datenbasis/Krebsdaten.csv', encoding="latin-1", sep=';', decimal=',')
 
+# Filter für die gewünschten Infos erstellen
+info =['Krebsart','Alter','Geschlecht','Prävalenzzeitraum']
 # Filter für die gewünschten Jahre erstellen
 jahre = ['2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']
-filtered_df = df.copy()
+
+#lamda bedeutet auf jede Spalte umsetzen
+df[jahre] = df[jahre].apply(lambda x: pd.to_numeric(x, errors='coerce'))
 
 # Die 5 höchsten Werte für jede Spalte ermitteln
-#lamda bedeutet auf jede Spalte umsetzen
-filtered_df[jahre] = filtered_df[jahre].apply(lambda x: pd.to_numeric(x, errors='coerce'))
-
-# Spalten in den Datentyp 'float' umwandeln
-top_5_values = filtered_df[jahre].max().sort_values(ascending=False)[:5]
+top_5_values = df[jahre].max().sort_values(ascending=False)[:5]
 
 # Ergebnis anzeigen
-print(top_5_values)
+print(df[info],df[jahre])
